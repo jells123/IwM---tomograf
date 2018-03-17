cc86011e3208a5cfeb4d984b3c04c5dafe3c5a6e
Ula Walińska <akane96@onet.pl>
2018-03-17 18:10:34 +0100

    updated

--- mainGUI.py
+++ mainGUI.py
@@ -1,7 +1,9 @@
+from scipy.fftpack import fft, ifft, fftfreq
 from scipy import misc
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from skimage.transform import radon
 from scipy import ndimage
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -27,9 +29,9 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 750, 500)
+        self.setGeometry(300, 300, 500, 600)
         self.setWindowTitle('Tomograf')
-        self.setWindowIcon(QIcon('t.png'))
+        self.setWindowIcon(QIcon('minilogan.png'))
 
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.white)
@@ -56,10 +58,10 @@ class Example(QWidget):
         l_text.setAlignment(Qt.AlignCenter)
 
         self.spin1.setMinimum(0.01)
-        self.spin1.setSingleStep(0.01)
+        self.spin1.setSingleStep(0.001)
 
         self.spin2.setMinimum(1)
-        self.spin2.setSingleStep(1)     # rozmiar obrazka?
+        self.spin2.setSingleStep(1)
         self.spin2.setMaximum(999)
 
         self.spin3.setMinimum(1)
@@ -88,7 +90,6 @@ class Example(QWidget):
         self.center()
         self.show()
 
-
     @pyqtSlot()
     def browseFiles(self):
         filter = "Png File (*.png)"
@@ -97,16 +98,23 @@ class Example(QWidget):
         self.name.setText(self.file.split("/")[-1])
         self.name.show()
         self.btn2.setEnabled(True)
+
+        a, b, c = start(self.file)
+        self.spin1.setValue(a)
+        self.spin2.setValue(b)
+        self.spin3.setValue(c)
+
         self.update()
 
     @pyqtSlot()
     def tomograph(self):
         self.waiter.show()
-        print(self.waiter.isVisible())          # nie dziala, lol...
+        print(self.waiter.isVisible())  # nie dziala, lol...
         print(self.waiter.text())
         self.update()
-        start(self.file, True, float(self.spin1.text().replace(',', '.')), int(self.spin2.text()), float(self.spin3.text().replace(',', '.')))
-        self.pic.setPixmap(QPixmap(os.getcwd() + "/result.png").scaledToHeight(250))
+        doTomography(self.file, float(self.spin1.text().replace(',', '.')), int(self.spin2.text()),
+                     float(self.spin3.text().replace(',', '.')))
+        self.pic.setPixmap(QPixmap(os.getcwd() + "/result.png").scaledToHeight(350))
         self.pic.show()
         self.waiter.hide()
         self.update()
@@ -134,7 +142,7 @@ def getCirclePoint(radius, angle, x_origin, y_origin, degrees=True):
     return x, y
 
 
-def getBresenhamLine(x1, y1, x2, y2):
+def getBresenhamLine(x1, y1, x2, y2, max):
     result = []
 
     # https://gist.github.com/bert/1085538
@@ -145,15 +153,16 @@ def getBresenhamLine(x1, y1, x2, y2):
     sy = 1 if y1 < y2 else -1
     err = dx + dy
     while 1:
-        result.append([x1, y1])
+        if max not in [x1, y1]:
+            result.append([x1, y1])
         if x2 == x1 and y2 == y1:
             break
         e2 = 2 * err
         if e2 >= dy:
-            err += dy
+            err += dy;
             x1 += sx
         if e2 <= dx:
-            err += dx
+            err += dx;
             y1 += sy
 
     return result
@@ -167,18 +176,18 @@ def getSquarePoints(points, squarePoints):
     return result
 
 
-def start(file, podane=False, alpha=0.0, n=0, l=0.0):
+def start(file):
+    image = misc.imread(file, flatten=True).astype('float64')
+    return (180. / image.shape[0]), image.shape[0], 180.
+
+
+def doTomography(file, alpha, n, l):
     # Read image as 64bit float gray scale
     image = misc.imread(file, flatten=True).astype('float64')
     if image.shape[0] != image.shape[1]:
         print("Error! Image is not square.")
-    if podane:
-        doTomography(image, alpha, n, l)
-    else:
-        doTomography(image, (180. / image.shape[0]), image.shape[0], 180.)
 
-
-def doTomography(image, alpha, n, l):
+    print(image.shape)
 
     # alpha = 180. / image.shape[0]  # obrót tomografu
     # n = image.shape[0]  # number of emiters
@@ -186,6 +195,7 @@ def doTomography(image, alpha, n, l):
 
     cx, cy = np.float(image.shape[0] / 2), np.float(image.shape[1] / 2)
     radius = np.float(image.shape[0] / 2)
+    square_size = image.shape[0]
 
     angles = list(np.arange(0., 180., alpha, dtype=np.float32))
     emiters = list(np.linspace(-l / 2, l / 2, n, dtype=np.float32))
@@ -193,30 +203,41 @@ def doTomography(image, alpha, n, l):
     dist = l / n  # angle distance between emiters
     sinogramData = np.ndarray(shape=(len(angles), len(emiters)), dtype=np.float32)
 
+    image = np.array(image, dtype=np.float32)
+
+    circlePoints = {}
     for a, angle in enumerate(angles):
         for idx, e in enumerate(emiters):
-
             x1, y1 = getCirclePoint(radius, angle + e, cx, cy)
             x2, y2 = getCirclePoint(radius, angle + 180. - e, cx, cy)
+            circlePoints[(angle, e)] = [x1, y1, x2, y2]
 
-            pixels = getBresenhamLine(x1, y1, x2, y2)
-
-            sum = 0
-            for p, px in enumerate(pixels):
-                try:
-                    sum += image[px[0]][px[1]]
-                except IndexError:
-                    pass
-
+            pixels = getBresenhamLine(x1, y1, x2, y2, square_size)
+            sum = np.sum(list(map(lambda px: image[px[0]][px[1]], pixels)))
             sinogramData[a][idx] = sum
-    newImage = np.zeros(shape=image.shape, dtype=np.float32)
 
+    print("First iter done!")
+    newImage = np.zeros(shape=image.shape, dtype=np.float32)
     border = image.shape[0] - 1
+
+    closest_two = 2 ** np.ceil(np.log2(2 * sinogramData.shape[0]))
+    freqs_count = max(64, int(closest_two))  # nie może być mnie niż 64
+    pad_width = ((0, freqs_count - sinogramData.shape[0]), (0, 0))
+    sinogram_padded = np.pad(sinogramData, pad_width, mode='constant', constant_values=0)
+
+    f = fftfreq(freqs_count).reshape(-1, 1)
+    omega = 2 * np.pi * f
+    ramp_filter = 2 * np.abs(f)
+
+    projection = fft(sinogram_padded, axis=0) * ramp_filter
+    filtered_sgram = np.real(ifft(projection, axis=0))
+
+    sinogramData = filtered_sgram[:sinogramData.shape[0], :]
+
     for aa, angle in enumerate(angles):
         for idx, e in enumerate(emiters):
 
-            x1, y1 = getCirclePoint(radius, angle + e, cx, cy)
-            x2, y2 = getCirclePoint(radius, angle + 180. - e, cx, cy)
+            x1, y1, x2, y2 = circlePoints[(angle, e)]
 
             if int(x1) != int(x2) and int(y1) != int(y2):
                 a = (y2 - y1) / (x2 - x1)
@@ -229,7 +250,7 @@ def doTomography(image, alpha, n, l):
                 x4, y4 = (border - b) / a, border
 
                 points = [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
-                points = getSquarePoints(points, [0, image.shape[0] - 1, 0, image.shape[1] - 1])
+                points = list(filter(lambda x: 0 <= x[0] <= border and 0 <= x[1] <= border, points))
                 x1, y1 = points[0]
                 x2, y2 = points[1]
 
@@ -240,26 +261,35 @@ def doTomography(image, alpha, n, l):
                 x1 = 0
                 x2 = image.shape[0] - 1
 
-            pixels = getBresenhamLine(x1, y1, x2, y2)
+            pixels = getBresenhamLine(x1, y1, x2, y2, square_size)
+            if len(pixels) == 0:
+                print("#PRZYPAŁ")  # jeden przypadek dzielenia przez zero, lol
+                addValue = 0
+            else:
+                addValue = sinogramData[aa][idx] / len(pixels)
 
-            for px in pixels:
-                try:
-                    newImage[px[0]][px[1]] += sinogramData[aa][idx] / len(pixels)
-                except IndexError:
-                    # print("INDEX ERROR!")
-                    # print(px, aa, idx)
-                    pass
+            newImageFlat = np.reshape(newImage, newshape=-1)
+            newPixels = np.array(list(map(lambda x: x[1] + x[0] * image.shape[0], pixels)), dtype=np.uint16)
+            newImageFlat[newPixels] += addValue
+            newImage = np.reshape(newImageFlat, newshape=image.shape)
 
-    sinogramData /= sinogramData.sum()
-    sinogramData *= 255
+    # sinogramData /= sinogramData.sum()
+    # sinogramData *= 255
+    sinogramData /= np.max(sinogramData)
 
-    plt.figure(figsize=(20, 7))
+    xcenter, ycenter = np.float(image.shape[0] / 2), np.float(image.shape[1] / 2)
+
+    theta = np.linspace(0., 180., max(image.shape), endpoint=False)
+    correctSinogram = radon(image, theta=theta, circle=True)
+
     # Plot the original and the radon transformed image
-    plt.subplot(1, 3, 1), plt.imshow(image, cmap='gray')
+    plt.subplot(2, 2, 1), plt.imshow(image, cmap='gray')
     plt.xticks([]), plt.yticks([])
-    plt.subplot(1, 3, 2), plt.imshow(ndimage.rotate(sinogramData, -90), cmap='gray')
+    plt.subplot(2, 2, 2), plt.imshow(ndimage.rotate(sinogramData, -90), cmap='gray')
     plt.xticks([]), plt.yticks([])
-    plt.subplot(1, 3, 3), plt.imshow(newImage, cmap='gray')
+    plt.subplot(2, 2, 3), plt.imshow(newImage, cmap='gray')
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(2, 2, 4), plt.imshow(correctSinogram, cmap='gray')
     plt.xticks([]), plt.yticks([])
     # plt.show()
     plt.savefig("result.png")
